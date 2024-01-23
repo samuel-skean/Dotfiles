83549fea6cf686a142401fd58f7f29593d327c17
Samuel Skean <sskean2@uic.edu>
2024-01-22 19:29:24 -0600

    Made nvim line numbers hybrid.

--- .config/nvim/lua/samuelskean/init.lua
+++ .config/nvim/lua/samuelskean/init.lua
@@ -1,3 +1,7 @@
 require("samuelskean.remap")
 require("samuelskean.fourSpaceTabs")
+
+-- Enable hybrid line numbers (absolute number on the current line,
+-- relative number elsewhere):
+vim.wo.number = true
 vim.wo.relativenumber = true
