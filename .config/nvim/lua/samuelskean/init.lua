2016ea0ed73f14f3c4c95fa89ee7bef6fd9939d7
Samuel Skean <sskean2@uic.edu>
2024-01-24 00:32:45 -0600

    Made the options global options - I think. This fixed an inconsistency with how trailing whitespace was displayed, as it was displayed differently when nvim was opened on a file vs on a directory. Now, it works the same in both. No other observed changes.

--- .config/nvim/lua/samuelskean/init.lua
+++ .config/nvim/lua/samuelskean/init.lua
@@ -3,9 +3,10 @@ require("samuelskean.fourSpaceTabs")
 
 -- Enable hybrid line numbers (absolute number on the current line,
 -- relative number elsewhere):
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Enable showing dots on trailing whitespace:
-vim.wo.listchars = "trail:·"
-vim.wo.list = true
+vim.o.listchars = "trail:·"
+vim.o.list = true
+
