ce47f62bbae2786d9545c56d74e92c2cc493d506
Samuel Skean <sskean2@uic.edu>
2025-06-27 09:40:03 -0500

    Tiny typo fix.
    What I get for not testing! D'oh.

--- .samuel_skean_dotfiles_miscellanea/install_git_hooks.bash
+++ .samuel_skean_dotfiles_miscellanea/install_git_hooks.bash
@@ -6,5 +6,5 @@ git --work-tree="${HOME}" --git-dir="${HOME}/Dotfiles.git" config core.hooksPath
 rm ~/Dotfiles.git/hooks/*.sample
 cat > ~/Dotfiles.git/hooks/README.md <<'EOF'
 This isn't where Dotfiles.git's core.hooksPath is set to.
-See `~/.samuel_skean_dotfiles_miscellanea/install_git_hooks.bash` for more info."
+See `~/.samuel_skean_dotfiles_miscellanea/install_git_hooks.bash` for more info.
 EOF
