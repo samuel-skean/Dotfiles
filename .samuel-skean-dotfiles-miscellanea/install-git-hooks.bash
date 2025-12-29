f8c1d9721f95d8ffe9e99c7d6fceeec69d9341f9
Samuel Skean <sskean2@uic.edu>
2025-12-29 09:39:12 -0800

    Use find to delete sample commit hooks everywhere.

--- .samuel-skean-dotfiles-miscellanea/install-git-hooks.bash
+++ .samuel-skean-dotfiles-miscellanea/install-git-hooks.bash
@@ -3,7 +3,7 @@
 set -eux
 
 git --work-tree="${HOME}" --git-dir="${HOME}/Dotfiles.git" config core.hooksPath "${HOME}/.samuel-skean-dotfiles-miscellanea/git-hooks"
-rm ~/Dotfiles.git/hooks/*.sample
+find {{git_dir}}/hooks/ -name "*.sample" -delete
 cat > ~/Dotfiles.git/hooks/README.md <<'EOF'
 This isn't where Dotfiles.git's core.hooksPath is set to.
 See `~/.samuel-skean-dotfiles-miscellanea/install-git-hooks.bash` for more info.
