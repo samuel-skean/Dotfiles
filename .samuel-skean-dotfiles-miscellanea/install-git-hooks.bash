36dcfaacecb1e498e0ab2127b583245ca82de2b5
Samuel Skean <sskean2@uic.edu>
2025-12-29 09:42:02 -0800

    Uh, why did I do that.

--- .samuel-skean-dotfiles-miscellanea/install-git-hooks.bash
+++ .samuel-skean-dotfiles-miscellanea/install-git-hooks.bash
@@ -3,7 +3,7 @@
 set -eux
 
 git --work-tree="${HOME}" --git-dir="${HOME}/Dotfiles.git" config core.hooksPath "${HOME}/.samuel-skean-dotfiles-miscellanea/git-hooks"
-find {{git_dir}}/hooks/ -name "*.sample" -delete
+find ${HOME}/Dotfiles.git/hooks/ -name "*.sample" -delete
 cat > ~/Dotfiles.git/hooks/README.md <<'EOF'
 This isn't where Dotfiles.git's core.hooksPath is set to.
 See `~/.samuel-skean-dotfiles-miscellanea/install-git-hooks.bash` for more info.
